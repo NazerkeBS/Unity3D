31267adb9f04a715f49b5fff60b20e7ec1ab64cd
Nazerke Seidan <naz@Nazerkes-MacBook-Pro.local>
2018-12-22 21:33:33 +0100

    unity 3d models rotations and switches

---
+++ QuizGame/Assets/Scripts/RotateModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateModel : MonoBehaviour
+{
+  
+
+    // rotate the model by z-axis
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, Time.deltaTime * 50));
+    }
+}
